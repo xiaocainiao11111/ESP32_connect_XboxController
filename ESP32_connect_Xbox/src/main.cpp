06906a04caac7465ef7e55a7497f988f8082aecb
cainiao <321489189@qq.com>
2024-08-20 19:23:50 +0800

    cainiao

--- ESP32_connect_Xbox/src/main.cpp
+++ ESP32_connect_Xbox/src/main.cpp
@@ -33,6 +33,99 @@ String xbox_string()
   return str;
 };
 
+/*
+支持四种振动模式
+left：上左电机动
+right：上右电机动
+center：下左电机和下右电机一起动，频率高力量小
+shake：下左电机和下右电机一起动，频率低力量大
+
+测试结果：
+四种模式都可以调振动力度
+下左电机和下右电机是绑定的，只能一起动，但是提供了两种振动模式，个人猜测是两种模式的原理是给电机不同的电压
+可以随意搭配使用，但center和shake一起用的话执行的应该是shake
+*/
+
+// 配置参考
+// repo.v.select.center = 0;
+// repo.v.select.left = 0;
+// repo.v.select.right = 0;
+// repo.v.select.shake = 0;
+// repo.v.power.center = 0; // x% power
+// repo.v.power.left = 0;
+// repo.v.power.right = 30;
+// repo.v.power.shake = 0;
+// repo.v.timeActive = 0; // 振动 x/100 秒，最大2.56秒(uint8_t)
+// repo.v.timeSilent = 0;   // 静止 x/100 秒
+// repo.v.countRepeat = 0;  // 循环次数 x+1
+
+void demoVibration()
+{
+  XboxSeriesXHIDReportBuilder_asukiaaa::ReportBase repo;
+  Serial.println("full power for 1 sec");
+  xboxController.writeHIDReport(repo);
+  delay(2000);
+
+  repo.v.select.center = true;
+  repo.v.select.left = false;
+  repo.v.select.right = false;
+  repo.v.select.shake = false;
+  repo.v.power.center = 30; // 30% power
+  repo.v.timeActive = 50;   // 0.5 second
+  Serial.println("run center 30\% power in half second");
+  xboxController.writeHIDReport(repo);
+  delay(2000);
+
+  repo.v.select.center = false;
+  repo.v.select.left = true;
+  repo.v.power.left = 30;
+  Serial.println("run left 30\% power in half second");
+  xboxController.writeHIDReport(repo);
+  delay(2000);
+
+  repo.v.select.left = false;
+  repo.v.select.right = true;
+  repo.v.power.right = 30;
+  Serial.println("run right 30\% power in half second");
+  xboxController.writeHIDReport(repo);
+  delay(2000);
+
+  repo.v.select.right = false;
+  repo.v.select.shake = true;
+  repo.v.power.shake = 30;
+  Serial.println("run shake 30\% power in half second");
+  xboxController.writeHIDReport(repo);
+  delay(2000);
+
+  repo.v.select.shake = false;
+  repo.v.select.center = true;
+  repo.v.power.center = 50;
+  repo.v.timeActive = 20;
+  repo.v.timeSilent = 20;
+  repo.v.countRepeat = 2;
+  Serial.println("run center 50\% power in 0.2 sec 3 times");
+  xboxController.writeHIDReport(repo);
+  delay(2000);
+}
+
+void demoVibration_2()
+{
+  XboxSeriesXHIDReportBuilder_asukiaaa::ReportBase repo;
+  static uint16_t TrigMax = XboxControllerNotificationParser::maxTrig;
+  String str_1;
+  repo.setAllOff();
+  repo.v.select.left = true;
+  repo.v.select.right = true;
+  repo.v.power.left = (uint8_t)((float)xboxController.xboxNotif.trigLT / (float)TrigMax * 100);
+  repo.v.power.right = (uint8_t)((float)xboxController.xboxNotif.trigRT / (float)TrigMax * 100);
+  repo.v.timeActive = 50;
+  xboxController.writeHIDReport(repo);
+  str_1 = String(repo.v.power.left) + "," + String(repo.v.power.right) + "\n";
+
+  Serial.print(str_1);
+  delay(50);
+}
+
 void setup()
 {
   Serial.begin(115200);
@@ -51,7 +144,9 @@ void loop()
     }
     else
     {
-      Serial.print(xbox_string());
+      // Serial.print(xbox_string());
+      // demoVibration();
+      demoVibration_2();
     }
   }
   else
@@ -62,5 +157,4 @@ void loop()
       ESP.restart();
     }
   }
-  delay(20);
 }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ESP32_connect_XboxController
+演示视频：[【开源】ESP32通过蓝牙连接Xbox手柄读取信号](https://www.bilibili.com/video/BV163eKebExM/?spm_id_from=333.999.list.card_archive.click)
 
 
 
@@ -28,7 +29,9 @@
 
 * 需要在代码中替换成自己的xbox手柄蓝牙MAC地址，方法如下：
   >1、用电脑蓝牙连接手柄
+  >
   >2、打开设备管理器，找到：蓝牙->Xbox Wireless controller->双击打开->详细信息->属性栏下拉->找到“蓝牙设备地址”->得到地址
+  >
   >3、把地址替换代码对应位置 `XboxSeriesXControllerESP32_asukiaaa::Core
     xboxController("f4:6a:d7:a0:5c:59");`
 
@@ -37,7 +40,20 @@
 * ### **注意**：
   > 把代码烧录到开发板后，首次连接手柄的时候需要长按手柄顶部的小按钮，让手柄进入配对模式，这时候西瓜灯会闪的飞快，然后就等它跟ESP32配对，成功后第二次就可以直接连了。
   
+## 2. DLC
+  >2024/8/20更新：支持手柄振动
+  >[源例程参考](https://github.com/asukiaaa/arduino-XboxSeriesXControllerESP32/tree/main/examples/vibration)
 
+  >#### 测试结果：（详看代码，欢迎补充和更正）
+  >支持四种振动模式
+  >left：上左电机动
+  >right：上右电机动
+  >center：下左电机和下右电机一起动，频率高力量小
+  >shake：下左电机和下右电机一起动，频率低力量大
+  >
+  >1、四种模式都可以调振动力度
+  >2、下左电机和下右电机是绑定的，只能一起动，但是提供了两种振动模式，个人猜测是两种模式的原理是同给电机不同的电压
+  >3、可以随意搭配使用，但center和shake一起用的话执行的应该是shake
 
 
 ---
